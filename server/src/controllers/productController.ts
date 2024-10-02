52a5831127e90aabccec248e3eca956e64e3ee47
Elias Shemsu <echosierran@gmail.com>
2024-10-02 13:51:26 +0300

    Product controller

---
+++ server/src/controllers/productController.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getProducts = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    try {
+        const search = req.query.search?.toString();
+        const products = await prisma.products.findMany({
+            where: {
+                name: {
+                    contains: search,
+                }
+            }
+        })
+        res.json(products)
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong", error });
+    }
+}
+
+export const createProduct = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    try {
+        const { productId, name, price, rating, stockQuantity } = req.body;
+        //validation
+
+        const product = await prisma.products.create({
+            data: {
+                productId,
+                name,
+                price,
+                rating,
+                stockQuantity
+            }
+        })
+        res.status(201).json(product)
+    }
+    catch (error) {
+        res.status(500).json({ message: "Couldn't save new product", error })
+    }
+}
